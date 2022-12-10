c2ffa29b060e4dcea7432b241d1fec43a477c25c
Antonio Carlos <antoniocf1505@gmail.com>
2022-12-10 22:43:08 +0000

    codigos var e conversoes

---
+++ types-variables-functions/02-variables/convertions.js
@@ -0,0 +1,12 @@
+// tipo de dado
+// booleanos
+
+// conversão implicita
+const number = 250
+const numberString = Number("250");
+
+//Number()
+//String()
+console.log(number + Number(numberString))
+
+// conversão explicita
---
+++ types-variables-functions/02-variables/truthy-falsy.js
@@ -0,0 +1,25 @@
+ // bolean
+//  const usurarioLogado = true
+//  const contapaga = false
+ 
+// // truthy or falsy
+
+// // 0 => false
+// // 1 => true
+
+// console.log(0 == false)
+// console.log("" == false)
+// console.log(1 == true)
+
+// undefined
+
+// null ==> vazio ou nada 
+
+let minhaVar;
+let varNull = null;
+
+let numero = 3;
+let string = "Alura";
+
+console.log(typeof minhaVar)
+console.log(typeof varNull)
